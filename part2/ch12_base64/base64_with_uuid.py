a627f00837c18c3f15b77f762235563a79964ba8
deepscape <hdkim.scala@gmail.com>
2021-07-08 11:01:48 +0900

    chapter 12. base64

---
+++ part2/ch12_base64/base64_with_uuid.py
@@ -0,0 +1,13 @@
+import base64
+import uuid
+
+
+my_uuid = uuid.uuid4()
+print('원본 UUID={0}, 바이트 길이={1}'.format(str(my_uuid), len(my_uuid.bytes)))
+b64_encoded_str = base64.b64encode(my_uuid.bytes)
+print('base64 인코딩 문자열=\'{0}\', 바이트 길이={1}'.format(
+    b64_encoded_str.decode('utf-8'), len(b64_encoded_str)))
+
+
+decoded_uuid = uuid.UUID(bytes=base64.b64decode(b64_encoded_str))
+print('base64 디코딩 된 UUID={0}'.format(decoded_uuid))
