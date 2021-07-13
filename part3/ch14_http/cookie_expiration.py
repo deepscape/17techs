536c0920d3f373938094ef28e78cf4a0db445d0c
deepscape <hdkim.scala@gmail.com>
2021-07-13 15:30:07 +0900

    ch14. session tracking mode

--- part3/ch14_http/cookie_expiration.py
+++ part3/ch14_http/cookie_expiration.py
@@ -23,3 +23,7 @@ def hello_world():
 
 
 app.run()
+
+# https://thecodinglog.github.io/web/2020/08/11/what-is-session.html
+# 서버가 클라이언트 ID를 어떤 방법으로 추적할 것인지 정의한 것을 세션 트래킹 모드라고 합니다.
+# 트래킹 모드에는 쿠키 사용 모드, URL Rewriting 모드, SSL 모드 가 있는데 대부분 서버에서 쿠키 사용 모드를 기본값으로 하고 있고 이 모드 사용을 권장하고 있습니다.
