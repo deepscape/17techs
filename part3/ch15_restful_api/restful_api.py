075de08413f4ec085805db0c6dc661dfbbefd957
deepscape <hdkim.scala@gmail.com>
2021-07-14 21:27:18 +0900

    ch15. restful_api finish

--- part3/ch15_restful_api/restful_api.py
+++ part3/ch15_restful_api/restful_api.py
@@ -38,6 +38,88 @@ def write_post():
     return 'OK', 200
 
 
+@bp.route('/posts/<number>', methods=['GET'])
+def get_post(number):
+    post = posts.get(int(number), None)
+    if not post:
+        return 'Bad Request', 400
+
+    posts_json = [{'title': post.title,
+                   'contents': post.contents,
+                   'date': post.date,
+                   'number': number}]
+    response_json = {'post': posts_json}
+
+    try:
+        # ensure_ascii=False 로 지정하여 유니코드가 포함된다는 것을 명시
+        return json.dumps(response_json, ensure_ascii=False)
+    except json.JSONDecodeError:
+        return 'Internal Server Error', 500
+
+
+@bp.route('/posts', methods=['GET'])
+def get_posts():
+    posts_size = request.args.get('size', '-1')
+    posts_size = int(posts_size)
+
+    posts_json = []
+    posts_acquired = 0
+    # 글을 가져옵니다.
+    for number in posts:
+        post = posts[number]
+        posts_json.append({'title': post.title,
+                           'contents': post.contents,
+                           'date': post.date,
+                           'number': number})
+        # 글 개수가 지정 개수를 넘었을 경우 더 이상 가져오지 않습니다.
+        posts_acquired = posts_acquired + 1
+        if 0 <= posts_size <= posts_acquired:
+            break
+
+    response_json = {'posts': posts_json}
+
+    try:
+        # ensure_ascii=False 로 지정하여 유니코드가 포함된다는 것을 명시합니다.
+        return json.dumps(response_json, ensure_ascii=False)
+    except json.JSONDecodeError:
+        return 'Internal Server Error', 500
+
+
+@bp.route('/posts/<number>', methods=['PUT'])
+def update_post(number):
+    number = int(number)
+    post = posts.get(number, None)
+    if not post:
+        return 'Bad Request', 400
+
+    # 변경할 제목과 내용을 가져옵니다.
+    request_json = request.get_json()
+    title = request_json.get('title', '')
+    contents = request_json.get('contents', '')
+
+    if len(title) == 0 or len(contents) == 0:
+        return 'Bad request', 400
+
+    # 마지막 수정 날짜를 업데이트합니다.
+    now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print('title={0}, contents={1}, date={2}, number={3}'.format(
+        title, contents, now, number))
+
+    # 변경된 내용으로 전체 데이터를 덮어씁니다.
+    posts[number] = BlogPost(title=title, contents=contents, date=now)
+    return 'OK', 200
+
+
+@bp.route('/posts/<number>', methods=['DELETE'])
+def delete_post(number):
+    post = posts.get(int(number), None)
+    if not post:
+        return 'Bad Request', 400
+
+    del posts[int(number)]
+    return 'OK', 200
+
+
 app = Flask(__name__)
 app.register_blueprint(bp)
 app.url_map.strict_slashes = False
